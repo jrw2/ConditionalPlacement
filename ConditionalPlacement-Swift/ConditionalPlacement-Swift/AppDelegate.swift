5bc66be82a6fd091cc715168ef6c09ce54fd94f2
James Westmoreland <w7jr@me.com>
2014-08-24 09:45:16 -0600

    Added code and test.

--- ConditionalPlacement-Swift/ConditionalPlacement-Swift/AppDelegate.swift
+++ ConditionalPlacement-Swift/ConditionalPlacement-Swift/AppDelegate.swift
@@ -18,7 +18,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         // Insert code here...
         
+        var karl = 10
+        var leonard = 7
+        var karlPlace = karl > 7 ? 1 : 2
+        var leonardPlace = leonard > 7  ? 1 : 2
         
+        if (karlPlace == 1 && leonardPlace == 1) {
+            NSLog("Both teachers are placed 1")
+        } else {
+            if (karlPlace == 1) {
+                NSLog("Karl is placed 1 and Leonard is placed 2")
+            } else if (leonardPlace == 1) {
+                NSLog("Leonard is placed 1 and Karl is placed 2")
+            } else {
+                NSLog("Karl and Leonard are placed 2" )
+            }
+        }
         
         return true
     }
